35862fef7d179280b4c221f3aba71d76ef8bc396
Anwesh43 <anweshthecool0@gmail.com>
2021-01-20 23:27:59 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedbranchgrowview/MainActivity.kt
+++ app/src/main/java/com/example/linkedbranchgrowview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedbranchgrowview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.branchgrowview.BranchGrowView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BranchGrowView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
