75820179698bf300663b33cc0bd550240c1b4702
Anwesh43 <anweshthecool0@gmail.com>
2021-01-20 14:14:56 +0530

    adding constants and libraries

--- branchgrowview/src/main/java/com/example/branchgrowview/BranchGrowView.kt
+++ branchgrowview/src/main/java/com/example/branchgrowview/BranchGrowView.kt
@@ -1,2 +1,27 @@
 package com.example.branchgrowview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#673AB7",
+    "#FF9800",
+    "#795548",
+    "#2196F3"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val branches : Int = 5
+val sizeFactor : Float = 2.9f
+val lFactor : Float = 8.1f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
