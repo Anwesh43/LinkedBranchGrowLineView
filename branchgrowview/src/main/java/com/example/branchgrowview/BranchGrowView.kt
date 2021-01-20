7494b8b754f9cd582a0e601ecd1555a2ced4ea1a
Anwesh43 <anweshthecool0@gmail.com>
2021-01-20 22:46:58 +0530

    adding BranchGrowView class

--- branchgrowview/src/main/java/com/example/branchgrowview/BranchGrowView.kt
+++ branchgrowview/src/main/java/com/example/branchgrowview/BranchGrowView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawBGNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBranchGrow(scale, w, h, paint)
 }
+
+class BranchGrowView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
